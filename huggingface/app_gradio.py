8db1a1f508e5cc0aa14faac5db42d9bf3edf0198
LuisMiguel1238907 <133611002+LuisMiguel1238907@users.noreply.github.com>
2025-11-21 17:13:21 -0500

    Update app_gradio.py

--- huggingface/app_gradio.py
+++ huggingface/app_gradio.py
@@ -1,1 +1,21 @@
+import gradio as gr
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
+
+def predict(text):
+    output = classifier(text)[0]
+    label = output["label"]
+    score = round(output["score"], 3)
+    return f"Predicción: {label} (confianza: {score})"
+
+demo = gr.Interface(
+    fn=predict,
+    inputs="text",
+    outputs="text",
+    title="Clasificador de Sentimiento",
+    description="Modelo preentrenado de Hugging Face para clasificar texto."
+)
+
+demo.launch()
 
